8ace39756c50ec4c8dc04f952ab991e7c4b34154
Pengfei Xiao <pengfei123xiao@gmail.com>
2020-05-10 16:43:20 +1000

    update baseURL;

--- src/api/weather-api.js
+++ src/api/weather-api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // create an new axios instance about open-weather-map
 export default axios.create({
-  baseURL: 'http://api.openweathermap.org/data/2.5/'
+  baseURL: 'https://api.openweathermap.org/data/2.5/'
 })
